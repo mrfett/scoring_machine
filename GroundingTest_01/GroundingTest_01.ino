e8c6478682e3e98f56ee252252f0707386ca40c8
bj.self <bj.self@asynchrony.com>
2015-01-13 21:19:33 -0600

    Updated grounding test with working code.  Added a resistor to ground to fencer1_a, fencer1_b, and fencer2_c

--- GroundingTest_01/GroundingTest_01.ino
+++ GroundingTest_01/GroundingTest_01.ino
@@ -10,45 +10,51 @@
 
  */
 
-// constants won't change. They're used here to 
-// set pin numbers:
-const int fencer1_a = 2;         // A Line for Fencer 1
-const int fencer1_b = 3;         // B Line for Fencer 1
-//const int fencer1_c = 4;         // C Line for Fencer 1
+// Constants won't change.
+const int fencer1_a = 2;             // A Line for Fencer 1
+const int fencer1_b = 3;             // B Line for Fencer 1
+//const int fencer1_c = 4;           // C Line for Fencer 1
 
-//const int fencer2_a = 6;         // A Line for Fencer 2
-//const int fencer2_b = 7;         // B Line for Fencer 2
-const int fencer2_c = 8;         // C Line for Fencer 2
+//const int fencer2_a = 6;           // A Line for Fencer 2
+//const int fencer2_b = 7;           // B Line for Fencer 2
+const int fencer2_c = 8;             // C Line for Fencer 2
 
-const int fencer1_touch = 10;         // LED when fencer1 Hits
+const int fencer1_light = 10;        // LED when fencer1 Hits
+//const int fencer1_light = 11;      // LED when fencer2 Hits
+//const int buzzer = 12;             // Buzzer Pin
 
-const boolean debug = true;             // Set to 1 to use the serial debugger
-const int onboard_led = 13;      // Onboard LED for testing
+const int light_length = 1;          // Length light should show (in seconds)
+
+const int onboard_led = 13;          // Onboard LED for testing
+
+const boolean debug = true;          // Set to true to use the serial debugger
+
+// Variables will change as the program runs
+boolean fencer1_hit_state = false;    // State of the fencer1 B line
+//boolean fencer2_hit_state = false;    // State of the fencer2 B line
+
+//boolean fencer1_ground_state = false; // State of the fencer2 C line
+boolean fencer2_ground_state = false; // State of the fencer1 C line
 
 
 void setup() {
-  
-  // initialize buttons
-  pinMode(fencer1_a, INPUT);
-  digitalWrite(fencer1_a, HIGH);
-  
-  //pinMode(fencer1_b, INPUT_PULLUP);
-  
-  //pinMode(fencer2_c, INPUT_PULLUP);
-
-  // initialize the lights:
-  pinMode(fencer1_touch, OUTPUT);
-  digitalWrite(fencer1_touch, LOW);
-  
-  pinMode(onboard_led, OUTPUT);
-  digitalWrite(onboard_led, LOW);
-
   // Set up debugging
   if (debug) {
     Serial.begin(9600);
-    Serial.println("Hello world");
-    delay(5000);// Give reader a chance to see the output.
   }
+  
+  // initialize buttons
+  pinMode(fencer1_a, OUTPUT);
+  digitalWrite(fencer1_a, HIGH);
+  
+  pinMode(fencer1_b, INPUT);
+  
+  pinMode(fencer2_c, INPUT);
+
+  // initialize the lights:
+  pinMode(fencer1_light, OUTPUT);
+  
+  pinMode(onboard_led, OUTPUT);
   
   // Signal when setup is done
   for (int i=0; i < 5; i++) {
@@ -60,30 +66,38 @@ void setup() {
 }
 
 void loop(){
-  // check if the pushbutton is pressed.
-  if (digitalRead(fencer1_a == LOW)) {
-    if(debug) {Serial.println('Button Pressed!');}
-    digitalWrite(onboard_led, HIGH);
-    /*
+  Serial.println("== Loop Start ==");
   
-    if (digitalRead(fencer1_b) == HIGH) {
-      if(debug) {Serial.println('No Ground');}
-      digitalWrite(fencer1_touch, HIGH);
-    } else {
-      digitalWrite(fencer1_touch, LOW);
-    }
-    if (digitalRead(fencer2_c) == LOW) {
-      if(debug) {Serial.println('***** GROUNDED!!!!');}
-      digitalWrite(onboard_led, HIGH);
-    } else {
-      digitalWrite(onboard_led, LOW);
-    }
-    */
-  } 
-  else {
-    if (debug) {Serial.println('No Press');}
-    // Clear any lights that have been lit
-    digitalWrite(fencer1_touch, LOW);
+  // Check Hit and Ground States
+  if (digitalRead(fencer1_b) == HIGH) {
+    fencer1_hit_state = true;
+  } else {
+    fencer1_hit_state = false;
+  }
+  if (digitalRead(fencer2_c) == HIGH) {
+    fencer2_ground_state = true;
+  } else {
+    fencer2_ground_state = false;
+  }
+  
+  if (fencer2_ground_state && fencer1_hit_state) {
+    Serial.println("Hit and Ground");
+    digitalWrite(onboard_led, HIGH);
+    digitalWrite(fencer1_light, LOW);
+    delay(light_length * 1000);
+  } else if (fencer2_ground_state) {
+    Serial.println("Ground Only");
+    digitalWrite(onboard_led, HIGH);
+    digitalWrite(fencer1_light, HIGH);
+    delay(light_length * 1000);
+  } else if (fencer1_hit_state) {
+    Serial.println("Hit Only");
     digitalWrite(onboard_led, LOW);
+    digitalWrite(fencer1_light, HIGH);
+    delay(light_length * 1000);
+  } else {
+    Serial.println("Neither Hit nor Ground");
+    digitalWrite(onboard_led, LOW);
+    digitalWrite(fencer1_light, LOW);
   }
 }
