6db72cb32e2fcd3d7619b29d38401fd7b27862f7
Matt Todd <matt.c.todd@gmail.com>
2015-01-19 22:55:18 -0600

    refactored a bit more the large conditional

--- src/scoreing_machine.cpp
+++ src/scoreing_machine.cpp
@@ -2,51 +2,58 @@
 #include "configurations.h"
 #include "lines.h"
 
-// Variables will change as the program runs
-boolean fencer1_hit_state = false;      // State of the fencer1 B line
-//boolean fencer2_hit_state = false;      // State of the fencer2 B line
+void hit_and_ground() {
+   Serial.println("Hit and Ground");
+   digitalWrite(onboard_led, HIGH);
+   digitalWrite(fencer1_light, LOW);
+   delay(debounce);
+}
 
-//boolean fencer1_ground_state = false; // State of the fencer2 C line
-boolean fencer2_ground_state = false; // State of the fencer1 C line
+void ground_only() {
+   Serial.println("Ground Only");
+   digitalWrite(onboard_led, HIGH);
+   digitalWrite(fencer1_light, LOW);
+   delay(debounce);
+}
+
+void hit_only() {
+   Serial.println("Hit Only");
+   digitalWrite(onboard_led, LOW);
+   digitalWrite(fencer1_light, HIGH);
+   digitalWrite(buzzer, HIGH);
+   delay(buzzer_length * 1000);
+   digitalWrite(buzzer, LOW);
+   delay((light_length * 1000) - (buzzer_length * 1000));
+   digitalWrite(fencer1_light, LOW);
+}
+
+void not_hit_or_ground() {
+   //Serial.println("Neither Hit nor Ground");
+   //digitalWrite(onboard_led, LOW);
+   //digitalWrite(fencer1_light, LOW);
+}
+
+bool is_fencer2_grounded() {
+   return digitalRead(fencer2_c) == HIGH;
+}
+
+bool is_fencer1_hit() {
+   return digitalRead(fencer1_b) == HIGH;
+}
 
 void run() {
-   Serial.println("== Loop Start ==");
+   //Serial.println("== Loop Start ==");
+
+   bool fencer1_hit = is_fencer1_hit();
+   bool fencer2_grounded = is_fencer2_grounded();
    
-   // Check Hit and Ground States
-   if (digitalRead(fencer1_b) == HIGH) {
-      fencer1_hit_state = true;
+   if (fencer2_grounded && fencer1_hit) {
+      hit_and_ground();
+   } else if (fencer2_grounded) {
+      ground_only();
+   } else if (fencer1_hit) {
+      hit_only();
    } else {
-      fencer1_hit_state = false;
-   }
-   if (digitalRead(fencer2_c) == HIGH) {
-      fencer2_ground_state = true;
-   } else {
-      fencer2_ground_state = false;
-   }
-   
-   if (fencer2_ground_state && fencer1_hit_state) {
-      Serial.println("Hit and Ground");
-      digitalWrite(onboard_led, HIGH);
-      digitalWrite(fencer1_light, LOW);
-      delay(debounce);
-   } else if (fencer2_ground_state) {
-      Serial.println("Ground Only");
-      digitalWrite(onboard_led, HIGH);
-      digitalWrite(fencer1_light, LOW);
-      delay(debounce);
-   } else if (fencer1_hit_state) {
-      Serial.println("Hit Only");
-      digitalWrite(onboard_led, LOW);
-      digitalWrite(fencer1_light, HIGH);
-      digitalWrite(buzzer, HIGH);
-      delay(buzzer_length * 1000);
-      digitalWrite(buzzer, LOW);
-      delay((light_length * 1000) - (buzzer_length * 1000));
-      digitalWrite(fencer1_light, LOW);
-      
-   } else {
-      Serial.println("Neither Hit nor Ground");
-      digitalWrite(onboard_led, LOW);
-      digitalWrite(fencer1_light, LOW);
+      not_hit_or_ground();
    }
 }
